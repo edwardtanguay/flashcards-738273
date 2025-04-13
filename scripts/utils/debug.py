e2c12280c0288fa481000b749419a5db1a9c4661
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-13 18:47:56 +0200

    debug finished

--- scripts/utils/debug.py
+++ scripts/utils/debug.py
@@ -1,2 +1,9 @@
 def devlog(line: str):
+	"""
+	Output information in console in a uniform way.
+
+	Example:
+		devlog("no files are locked")
+		devlog(f"There are {len(flashcards)} flashcards.")
+	"""
 	print(f"DEBUG ### {line} ################################")
