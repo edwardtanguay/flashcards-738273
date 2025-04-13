4503b89267ac63db0d02707889db7b7e58d100c8
Edward Tanguay (windows) <edward@tanguay.info>
2025-04-13 18:54:19 +0200

    files works

--- scripts/parse-data.py
+++ scripts/parse-data.py
@@ -1,1 +1,8 @@
-print("this will parse the data")
+import utils.debug as debug
+import utils.files as files
+ 
+debug.devlog("get lines from file")
+lines = files.get_lines_from_file("../data/flashcards.txt")
+
+for i, line in enumerate(lines, 1):
+    print(f"{i:03d}: {line}")
